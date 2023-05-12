fb4707ebe28c2563b8ff994e446acf6b8b8c64aa
sergey <sergeipopof178@gmail.com>
2023-05-12 11:13:58 +0800

    middleNode # slow and fast pointers technic

--- easy/linked/list/MiddleNode.py
+++ easy/linked/list/MiddleNode.py
@@ -42,3 +42,31 @@ data1 = {
 }
 head1 = LinkedList.create_linked_list(data1)
 print(middleNode(head1).value)
+
+data3 = {
+    "head": "1",
+    "nodes": [
+        {"id": "1", "next": "1-2", "value": 1},
+        {"id": "1-2", "next": "1-3", "value": 2},
+        {"id": "1-3", "next": "2", "value": 3},
+        {"id": "2", "next": "3", "value": 4},
+        {"id": "3", "next": "3-2", "value": 5},
+        {"id": "3-2", "next": "3-3", "value": 6},
+        {"id": "3-3", "next": "4", "value": 7},
+        {"id": "4", "next": None, "value": 8}
+    ]
+}
+head3 = LinkedList.create_linked_list(data3)
+
+# slow and fast pointers technic
+# time O(n)
+# space O(1)
+def middleNode2(linkedList):
+    slow = linkedList
+    fast = linkedList
+    while slow is not None and fast is not None:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+print(middleNode2(head3).value)
