3a2ee7811b394dcd238fa3c4ccd0e9b8d66e81c2
sergey <sergeipopof178@gmail.com>
2023-05-18 10:40:54 +0800

    bubbleSort

---
+++ easy/sorting/BubbleSort.py
@@ -0,0 +1,22 @@
+
+# time O(n)
+# space O(1)
+def bubbleSort(array):
+    l = 0
+    r = len(array) - 1
+    while l < r:
+        if array[l] > array[l + 1]:
+            store = array[l + 1]
+            array[l + 1] = array[l]
+            array[l] = store
+
+        l += 1
+        if l == r:
+            l = 0
+            r -= 1
+
+    return array
+
+
+sortedArr = bubbleSort([8, 5, 2, 9, 5, 6, 3])
+print(sortedArr)
