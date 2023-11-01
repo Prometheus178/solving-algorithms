da293a97c94f91c3f3ec9d136a56753237f1c1b4
sergey <sergeipopof178@gmail.com>
2023-11-01 10:11:20 +0700

    Single Cycle Check fixed

--- easy/graphs/SingleCycleCheck.py
+++ easy/graphs/SingleCycleCheck.py
@@ -1,15 +1,18 @@
 def getNextIdx(current_idx, array):
     jump = array[current_idx]
     nextIdx = (current_idx + jump) % len(array)
-    return nextIdx
+    return nextIdx if nextIdx >= 0 else nextIdx + len(array)
 
-
+# Time O(n) | Space O(n)
 def hasSingleCycle(array):
     count = 0
     currentIdx = 0
-
+    visited = set()
     while count < len(array):
         currentIdx = getNextIdx(currentIdx, array)
+        if visited.__contains__(currentIdx):
+            return False
+        visited.add(currentIdx)
         count += 1
 
     return currentIdx == 0
@@ -17,3 +20,7 @@ def hasSingleCycle(array):
 
 array = [2, 3, 1, -4, -4, 2]
 print(hasSingleCycle(array))
+
+array1 = [1, -1, 1, -1]
+print(hasSingleCycle(array1))
+
